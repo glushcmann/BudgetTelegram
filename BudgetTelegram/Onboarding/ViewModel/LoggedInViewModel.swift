3b1c464822067b27314c8e24b5b7bdf7cc70ec67
Nick <nikita.glu@icloud.com>
2020-05-02 15:06:59 +0300

    delete useless code

---
+++ BudgetTelegram/Onboarding/ViewModel/LoggedInViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  LoggedInViewModel.swift
+//  BudgetTelegram
+//
+//  Created by Никита on 30.04.2020.
+//  Copyright © 2020 Nikita Glushchenko. All rights reserved.
+//
+
+//import Foundation
+//
+//class LoggedInViewModel {
+//
+//    let credentials: User
+//
+//    init(with credentials: User) {
+//        self.credentials = credentials
+//    }
+//
+//}
